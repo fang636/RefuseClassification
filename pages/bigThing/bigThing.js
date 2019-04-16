d430a4cf770ffa5731ec109a4fe31614801c28b8
凉皮 <1195188852@qq.com>
2019-04-16 10:53:07 +0800

    大物件申请

--- pages/bigThing/bigThing.js
+++ pages/bigThing/bigThing.js
@@ -1,66 +1,148 @@
 // pages/bigThing/bigThing.js
-Page({
 
+const app = getApp()
+
+Page({
   /**
    * 页面的初始数据
    */
   data: {
+    phoneNumber: '',
+    explain: ''
+  },
+  /**
+   * 联系方式改变事件
+   */
+  pnChangeHander: function(e) {
+    this.setData({
+      phoneNumber: e.detail.value
+    })
+  },
+  /**
+   * 说明文本改变事件
+   */
+  explainChangeHander: function(e) {
+    this.setData({
+      explain: e.detail.value
+    })
+  },
+  /**
+   *提交事件
+   */
+  submitHander: function(e) {
+    const _this = this
+    var phone = this.data.phoneNumber
+    if ((/(?:^1[3456789]|^9[28])\d{9}$/.test(phone))) {
+      wx.showModal({
+        title: '提示',
+        content: '是否确认提交大物件申请？',
+        success: function(res) {
+          if (res.confirm) {
+            wx.request({
+              header: {
+                'content-type': 'application/x-www-form-urlencoded',
+              },
+              method: 'POST',
+              url: app.globalData.url + 'djlj/weixin/sqdjlj',
+              data: {
+                djlj: _this.data.explain,
+                phone: _this.data.phoneNumber,
+                useropenId: app.globalData.openId
+              },
+              success: function(result) {
+                if (result.data) {
+                  wx.showToast({
+                    title: '操作成功',
+                    duration: 2000,
+                    icon: 'success'
+                  })
+                  wx.switchTab({
+                    url: '../index/index'
+                  })
+                } else {
+                  wx.showModal({
+                    title: '操作失败',
+                    content: '请检查是否已经存在一个大物件请求',
+                    showCancel: false
+                  })
+                }
+                console.log(result)
+              },
+              fail: function() {
 
+              }
+            })
+          } else {
+            return
+          }
+        }
+      })
+
+
+    } else {
+      wx.showToast({
+        title: '请填写正确的手机号',
+        icon: 'none'
+      })
+    }
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
+    this.setData({
+      phoneNumber: app.globalData.userModel.phone
+    })
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
